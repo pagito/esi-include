20dbafa62abaa674d5c71e366a03b3d606286f12
Giorgi Javrishvili <giorgi@codegear.io>
2020-12-06 20:37:59 +0400

    process all

--- proxy/server.js
+++ proxy/server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use('/', proxy('localhost:8000', {
 	userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
 		//console.log(proxyRes.headers['content-type'])
-		let headers = {};
   		//return proxyRes.headers['content-type'] == 'text/html' ? esi.process(proxyResData.toString()) : proxyResData;
 		return esi.process(proxyResData.toString());
 
